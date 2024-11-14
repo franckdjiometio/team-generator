88df5924aef690707aa4552090ca7021f3ec3d01
Franck Djiometio <franck.djiometio@gmx.de>
2024-11-14 22:42:13 +0100

    implement functionality to add new member

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'team-generator';
+  newMemberName = '';
+  members: string[] = [];
+
+  addMember() {
+    this.members.push(this.newMemberName);
+    console.log(this.members);
+  }
+
+  onInput(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
 }
